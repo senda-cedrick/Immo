a8b4f38bf8700e26d08002fedc5bd0ed998094e5
senda-cedrick <sendamutombo@gmail.com>
2026-05-20 08:09:08 +0100

    chore: ignorer définitivement les fichiers pycache et binaires

--- app_base/urls.py
+++ app_base/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
 from immo_travel.views import home_views
+from immo_travel.views.agence_views import AgenceCreateView, AgenceListView,AgenceDetailView, ListAgence
+
 
 urlpatterns = [
     path('', home_views.HomeView.as_view(), name='home'), # Cette route captera le http://127.0.0.1:8000/
     # Page show 
     path('agence/', views.agences, name='agences'),
-    path('agences/', views.garanties, name='garanties'),
+    path('lagence/',  ListAgence, name='lagences'),
+    path("agences/", AgenceListView.as_view(), name="agences"),
+    path('garanties/', views.garanties, name='garanties'),
     path('appartements/', views.appartements, name='appartements'),
     path('locataires/', views.locataires, name='locataires'),
     path('personnels/', views.personnels, name='personnels'),
